f1e484e88f1ddb151e9e076bfeb594029a7173b0
Piyush0692 <155516296+Piyush0692@users.noreply.github.com>
2025-08-29 12:43:33 +0530

    first commit

---
+++ src/components/Background.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <>
+      <div className='fixed z-[2] w-full h-screen'>
+        <div className='absolute flex justify-center w-full py-10 text-xl font-semibold text-zinc-500'>Documents.</div>
+        <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[12vw] leading-none tracking-tight font-semibold text-zinc-900'>Docs.</h1>
+        {}
+        <div className='absolute text-base font-medium -translate-x-1/2 bottom-6 left-1/2 text-zinc-500'>
+          Made  by Piyush
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Background
